f87802949a58b6692d5617f2d575dd6f1e16adcf
Jerome <j.patrick.wiz@gmail.com>
2016-02-06 14:28:45 +0800

    initial progress 2

---
+++ src/cs125/gonzalvo_marasigan/css/FCFS.java
@@ -0,0 +1,13 @@
+package cs125.gonzalvo_marasigan.css;
+
+import java.util.ArrayList;
+
+public class FCFS{
+	
+	private ArrayList<Process> processes;
+
+	public FCFS(ArrayList<Process> processes) {
+		this.processes = processes;
+	}
+
+}
--- src/cs125/gonzalvo_marasigan/css/Scheduler.java
+++ src/cs125/gonzalvo_marasigan/css/Scheduler.java
@@ -1,7 +1,41 @@
 package cs125.gonzalvo_marasigan.css;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Scheduler {
 	public static void main(String[] args){
-		
+		String sCsvFile = args[0];
+		BufferedReader br = null;
+		try {
+			br = new BufferedReader(new FileReader(sCsvFile));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			if(br != null){
+				try {
+					String line = null;
+					String splitter = ",";
+					ArrayList<Process> processes = new ArrayList<Process>();
+					while((line = br.readLine()) != null){
+						String[] row = line.split(splitter);
+						Process process = new Process(Integer.parseInt(row[0]), 
+								Integer.parseInt(row[1]), 
+								Integer.parseInt(row[2]), 
+								Integer.parseInt(row[3]));
+						processes.add(process);
+					}
+					FCFS fcfs = new FCFS(processes);
+					br.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
 	}
 }
